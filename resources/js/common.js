8ad87b71a5b3baef6eb75a75794cb5572d1d0a4c
Anatoly Zhuravlev <37895952+newstreetpunk@users.noreply.github.com>
2020-09-14 19:20:46 +0500

    banner adaptive

--- resources/js/common.js
+++ resources/js/common.js
@@ -5,4 +5,26 @@ jQuery(function($) {
 		$('.header__menu').toggleClass('open');
 	});
 
+	//Hide Menu on Scroll
+	var hideHeaderPos = 82;
+
+	/* опускающийся хедер при прокрутке вверх */
+	var prevScrollpos = window.pageYOffset;
+	$(window).scroll( function() {
+		var currentScrollPos = window.pageYOffset;
+		if (currentScrollPos > hideHeaderPos) {
+			if (prevScrollpos > currentScrollPos) {
+				document.getElementById("header").classList.remove("hide");
+				document.getElementById("header").classList.add("show");
+			} else {
+				document.getElementById("header").classList.remove("show");
+				document.getElementById("header").classList.add("hide");
+			}
+			prevScrollpos = currentScrollPos;
+				// $('.header').attr('style')
+			} else {
+				document.getElementById("header").classList.remove('show', 'hide');
+			}
+	});
+
 });
